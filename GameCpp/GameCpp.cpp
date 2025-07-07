fd8ff3eff89d95a6bd4eb5dcf42eb147cabcb8b5
KimJin0315 <xnxl159@naver.com>
2025-07-07 19:15:40 +0900

    (2025.07.07) 언오더셋

---
+++ GameCpp/GameCpp.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+#pragma region unordered_set
+
+    // std::unordered_set<const char*> unordered_set;
+    // 
+    // unordered_set.max_load_factor(2.0f);
+    // 
+    // unordered_set.insert("CookieRun");
+    // unordered_set.insert("MapleStory");
+    // unordered_set.insert("SuddenAttack");
+    // unordered_set.insert("OverWatch");
+    // unordered_set.insert("BattleGround");
+    // unordered_set.insert("Valorant");
+    // 
+    // cout << "Load Factor : " << unordered_set.load_factor() << endl;
+    // cout << "Bucket Count : " << unordered_set.bucket_count() << endl;
+    // 
+    // 
+    // for (const auto & element : unordered_set)
+    // {
+    //     cout << element << endl;
+    // }
+    // 
+    // unordered_set.clear();
+    // 
+    // cout << "unorder_set size : " << unordered_set.size();
+
+#pragma endregion
+
+    return 0;
+}
